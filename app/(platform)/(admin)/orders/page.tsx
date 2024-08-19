34ee68a2c832f704953198df11826a0b404bb729
HarryDang <danghuuphuc001@gmail.com>
2024-08-19 22:57:37 +0700

    reafactor(admin-orders): delete variable unused

--- app/(platform)/(admin)/orders/page.tsx
+++ app/(platform)/(admin)/orders/page.tsx
@@ -2,24 +2,7 @@ import { OrderClient } from "./order-client";
 
 import { AdminFloatingDock } from "./_orders-components/admin-floating-dock";
 
-export const progressValue = [
-  {
-    title: "$320",
-    description: "This month",
-    value: 320,
-    percentageDescription: " +25% from last week",
-    progressValue: 30,
-  },
-  {
-    title: "$320",
-    description: "This month",
-    value: 320,
-    // percentage: 12,
-    percentageDescription: " +10% from last week",
 
-    progressValue: 12,
-  },
-];
 
 export function OrderPage() {
   return (
