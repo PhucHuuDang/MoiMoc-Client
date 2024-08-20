5c9427c9c48fd1b666fec96e61242744786819d0
HarryDang <danghuuphuc001@gmail.com>
2024-08-21 00:52:04 +0700

    refactor(dashboard->orders) && feat(dashboard-products): move orders route to dashboard && create products route in dashboard

--- app/(platform)/(admin)/layout.tsx
+++ app/(platform)/(admin)/layout.tsx
@@ -2,23 +2,23 @@ import { Navbar } from "@/components/_global-components-reused/navbar";
 import { ContactModal } from "@/components/modal/contact-modal";
 import { LoginModal } from "@/components/modal/login-modal";
 import { RegisterModal } from "@/components/modal/register-modal";
-import { AdminSideBar } from "./orders/_orders-components/admin-sidebar";
-import { SheetSidebar } from "./orders/_orders-components/sheet-sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AdminSideBar } from "@/app/(platform)/(admin)/dashboard/orders/_orders-components/admin-sidebar";
+import { SheetSidebar } from "@/app/(platform)/(admin)/dashboard/orders/_orders-components/sheet-sidebar";
 
-const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+const AdminLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <TooltipProvider>
+    <TooltipProvider>
+      <div className="flex min-h-screen w-full flex-col">
         <AdminSideBar />
-      </TooltipProvider>
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <SheetSidebar />
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <SheetSidebar />
 
-        {children}
+          {children}
+        </div>
       </div>
-    </div>
+    </TooltipProvider>
   );
 };
 
-export default HomeLayout;
+export default AdminLayout;
