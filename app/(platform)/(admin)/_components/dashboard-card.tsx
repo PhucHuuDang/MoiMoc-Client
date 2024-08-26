7e2789e456d4bb8bc6482b6806bfadc9bdebdc4b
Harry Dang <danghuuphuc001@gmail.com>
2024-08-26 23:17:47 +0700

    feat(dashboard-page): chart statistics

---
+++ app/(platform)/(admin)/_components/dashboard-card.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+
+interface DashboardCardProps {
+  title: string;
+  description: string;
+  // value: number;
+  statistic: string;
+}
+
+export const DashboardCard = ({
+  description,
+  title,
+  statistic,
+  // value,
+  // percentageDescription,
+  // progressValue,
+}: DashboardCardProps) => {
+  return (
+    <Card x-chunk="dashboard-05-chunk-2 ">
+      <CardHeader className="pb-2">
+        <CardDescription>{description}</CardDescription>
+        <CardTitle className="text-4xl">{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="text-xs text-muted-foreground">{statistic}</div>
+      </CardContent>
+      {/*<CardFooter>*/}
+      {/*  <Progress value={progressValue} aria-label="12% increase" />*/}
+      {/*</CardFooter>*/}
+    </Card>
+  );
+};
---
+++ app/(platform)/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardClient } from "@/app/(platform)/(admin)/dashboard/dashboard-client";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <DashboardClient />
+    </>
+  );
+};
+
+export default DashboardPage;
