65387e754596e0af7f4bfd04c6449ea9f2d79350
HarryDang <danghuuphuc001@gmail.com>
2024-09-21 14:42:55 +0700

    fix: export OrderPage

--- app/(platform)/(admin)/dashboard/orders/page.tsx
+++ app/(platform)/(admin)/dashboard/orders/page.tsx
@@ -1,14 +1,12 @@
 import { OrderClient } from "./order-client";
 
-import { AdminFloatingDock } from "./_orders-components/admin-floating-dock";
-
-export function OrderPage() {
+const OrderPage = () => {
   return (
     <>
       <OrderClient />
       {/* <AdminFloatingDock /> */}
     </>
   );
-}
+};
 
 export default OrderPage;
